3d74005d5fac4831e419c4f29fb648782fe051c5
Ellie <ytaghavi@yahoo.com>
2021-02-22 20:46:42 -0800

    Project 04 Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,50 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+//load R0's value to a
+@R0
+D=M
+@a
+M=D
+
+//load R1's value to n
+@R1
+D=M
+@n
+M=D
+
+//set i to one
+@i
+M=0
+
+//set sum to zero
+@R3
+M=0
+
+(LOOP)
+    //check if i==n
+    @i
+    D=M
+    @n
+    D=D-M
+    @END
+    D;JEQ
+
+    //add a(R0) to R3
+    @a
+    D=M
+    @R3
+    M=D+M
+
+    //add 1 to i(iterate)
+    @i
+    M=M+1
+
+    //go back to the beggining of the loop
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
